754d6c8d3150ea82d60506b053f6f5780db5d79b
Volter3228 <vova.morozov3228@gmail.com>
2021-08-24 19:45:04 +0300

    Change migration classes names

--- database/migrations/2021_08_24_153627_create_place_types_table.php
+++ database/migrations/2021_08_24_153627_create_place_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatesPlaceTypesTable extends Migration
+class CreatePlaceTypesTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_08_24_154841_create_bookcases_table.php
+++ database/migrations/2021_08_24_154841_create_bookcases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatesBookcasesTable extends Migration
+class CreateBookcasesTable extends Migration
 {
     /**
      * Run the migrations.
