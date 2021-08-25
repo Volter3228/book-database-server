857c65faba7d4f181faf3954dcf895e510f9e546
Volter3228 <vova.morozov3228@gmail.com>
2021-08-25 17:35:34 +0300

    Refactor Place Controller

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -13,17 +13,16 @@ class PlaceController extends Controller
             ->map(function ($item) {
                 $item['type'] = 'bookcase';
                 $shelves = collect($item['shelves']);
-                $item['shelvesCount'] = $shelves->count();
-                $item['booksCount'] = $shelves->pluck('books')->count();
+                $item['shelves_count'] = $shelves->count();
+                $item['books_count'] = $shelves->pluck('books')->count();
 
                 return collect($item)->except(['shelves']);
-            })
-            ->except(['shelves']);
+            });
 
         $boxes = Box::with('books')->get()
             ->map(function ($item) {
             $item['type'] = 'box';
-            $item['booksCount'] = collect($item['books'])->count();
+            $item['books_count'] = collect($item['books'])->count();
             return collect($item)->except('books');
         });
         return $boxes->toBase()->merge($bookcases)->sortByDesc('updated_at')->values();
