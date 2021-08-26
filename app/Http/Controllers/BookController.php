69f22604d334280ced1f29869be7e76126c88dcb
Volter3228 <vova.morozov3228@gmail.com>
2021-08-26 18:14:19 +0300

    Create Book creation handler

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Models\Book;
-use Illuminate\Support\Facades\Log;
+use App\Models\Bookcase;
+use App\Models\Box;
+use App\Models\PlaceType;
+use App\Models\Shelf;
+use App\Models\Topic;
 
 class BookController extends Controller
 {
@@ -11,11 +16,76 @@ class BookController extends Controller
     {
         $books = Book::with(['authors', 'topics', 'placeType'])->get()->map(function ($item) {
             if ($item->placeType['name'] === 'shelf') {
-                Log::info("JOPA");
                 $item['shelf_number'] = $item->place()->number;
             }
             return collect($item)->except(['place_type']);
-        });
+        })->sortByDesc('created_at');
         return $books->values();
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'title' => 'required|max:255',
+            'authors' => 'required|array|min:1',
+            'authors.*' => 'required|string|min:1',
+            'topics' => 'required|array|min:1',
+            'topics.*' => 'required|string|min:1',
+            'placeId' => 'required|integer',
+            'placeType' => 'required',
+            'shelfNumber' => 'required_if:placeType,==,bookcase|min:1',
+            'description' => 'required',
+        ]);
+
+        [
+            'title' => $title, 'authors' => $authors, 'topics' => $topics, 'placeId' => $placeId,
+            'placeType' => $placeType, 'shelfNumber' => $shelfNumber, 'description' => $description
+        ] = $data;
+
+        if (!in_array($placeType, ['bookcase', 'box'])) {
+            return response()->json(['message' => 'Wrong Type'], 400);
+        }
+
+        if ($placeType === 'bookcase') {
+            $bookcase = Bookcase::findOrFail($placeId);
+            if (!$shelfNumber || $shelfNumber < 1 || $bookcase->shelves()->count() < $shelfNumber) {
+                return response()->json(['message' => 'Wrong shelves count'], 400);
+            }
+            $placeId = Shelf::where('bookcase_id', $placeId)->where('number', $shelfNumber)->first()->id;
+            $placeType = 'shelf';
+        } else {
+          $box = Box::findOrFail($placeId);
+        }
+
+        $placeTypeId = PlaceType::where('name', $placeType)->first()->id;
+
+        $book = Book::create([
+            'title' => $title,
+            'description' => $description,
+            'place_id' => $placeId,
+            'place_type_id' => $placeTypeId,
+        ]);
+
+        foreach($authors as $a) {
+            $author = Author::firstOrCreate(['name' => $a]);
+            $author->books()->attach($book->id);
+        }
+
+        foreach($topics as $t) {
+            $topic = Topic::firstOrCreate(['name' => $t]);
+            $topic->books()->attach($book->id);
+        }
+
+        $book = Book::with(['authors', 'topics', 'placeType'])
+            ->where('id', $book->id)
+            ->get()
+            ->map(function ($item) use ($shelfNumber) {
+                if ($item->placeType['name'] === 'shelf') {
+                    $item['shelf_number'] = $shelfNumber;
+                }
+            return collect($item)->except(['place_type']);
+        })->first();
+
+        return $book;
     }
 }
